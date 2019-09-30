27283df1ac2a23c280f363951ed5329a4fb5c49a
hongjongwan <serverwizard89@woowahan.com>
2019-09-30 23:45:52 +0900

    [요구사항 1] index.html 응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,11 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,10 +20,25 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+             OutputStream out = connection.getOutputStream()) {
+
+            String line = br.readLine();
+//            while (!line.isEmpty()) {
+//                System.out.println(line);
+//                line = br.readLine();
+//            }
+
+            String requestURI = line.split(" ")[1];
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body;
+            if ("/index.html".equals(requestURI)) {
+                body = Files.readAllBytes(new File("./webapp/index.html").toPath());
+            } else {
+                body = "Hello World".getBytes();
+            }
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -33,7 +46,8 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos,
+                                   int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
@@ -44,7 +58,8 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
+    private void responseBody(DataOutputStream dos,
+                              byte[] body) {
         try {
             dos.write(body, 0, body.length);
             dos.flush();
